2c052e8d18c0ba7ae0e16ef568cf386126beed7d
adogecheems <adogecheems@outlook.com>
2025-09-07 17:06:29 +0800

    ...

--- resources/fix.sh
+++ resources/fix.sh
@@ -1,4 +1,21 @@
-FIX_LIST=(
+DIR_FIX_LIST=(
+    zz_dockingstation
+    zz_pianokeys
+    zz_spritejsons
+)
+
+for target in "${DIR_FIX_LIST[@]}"; do
+    file="game/${target}.rpy"
+    sed -i \
+    -e 's/renpy\.config\.basedir\s*\+\s*"\s*\/game/renpy.config.gamedir + "/g' \
+    -e 's/config\.basedir\s*\+\s*"\s*\/game/config.gamedir + "/g' \
+    "$file"
+done
+
+sed -i  's/os\.path\.normcase\s*(\s*config\.basedir\s*\+\s*"\s*\/game\/"\s*)/config.gamedir/g' "game/chess.rpy"
+sed -zi 's/game_folder = os\.path\.normcase(\n[ \t]*renpy\.config\.basedir \+ "\/game\/"\n[ \t]*)/renpy.config.gamedir/g' "game/zz_dockingstation.rpy"
+
+REPLACE_LIST=(
     definitions
     script-affection
     script-ch30
@@ -16,7 +33,7 @@ FIX_LIST=(
     zz_pianokeys
 )
 
-for target in "${FIX_LIST[@]}"; do
+for target in "${REPLACE_LIST[@]}"; do
     file="game/${target}.rpy"
     sed -i \
     -e "s/renpy\.config\.basedir/store.home()/g" \
@@ -35,5 +52,9 @@ for target in "${REMOVE_LIST[@]}"; do
     rm -f "$file"
 done
 
-sed -i 's/build.include_update = True/build.include_update = False/' game/options.rpy
-sed -i 's/log_enable = True/log_enable = False/' renpy/config.py
+sed -i '/os\.chmod(config\.gamedir\.format(fp), 0755)/d' "game/chess.rpy"
+sed -i '/if not main_menu:/,/style "navigation_button"/d' "game/screen.rpy"
+sed -i '/if not mas_checked_update:/,+2d' "game/script-ch30.rpy"
+sed -i '/with open(basedir \+ "\/game\/masrun", "w") as versfile:/,+1d' "game/splash.rpy"
+sed -i '/build.include_update = True/build.include_update = False/' game/options.rpy
+sed -i '/log_enable = True/log_enable = False/' renpy/config.py
